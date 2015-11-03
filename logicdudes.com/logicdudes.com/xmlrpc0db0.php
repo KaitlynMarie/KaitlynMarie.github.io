f58bfd06def821c25562e80f84a3a184dcc0c0ef
Kaitlyn Ashcraft <kaitlynmariepowell@gmail.com>
2015-11-03 18:55:39 -0500

    ld test

---
+++ logicdudes.com/logicdudes.com/xmlrpc0db0.php
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+  <service>
+    <engineName>WordPress</engineName>
+    <engineLink>http://wordpress.org/</engineLink>
+    <homePageLink>https://logicdudes.com</homePageLink>
+    <apis>
+      <api name="WordPress" blogID="1" preferred="true" apiLink="https://logicdudes.com/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="https://logicdudes.com/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://logicdudes.com/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="https://logicdudes.com/xmlrpc.php" />
+          </apis>
+  </service>
+</rsd>
